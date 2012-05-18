e3631d2e035b29c5c5c3a37df939f979c65fbbb5
Xi Wang <xi@mit.edu>
2012-05-17 23:29:00 -0400

    add basic testing infrastructure
    Move away from autoconf's autotest.  The new testing framework is
    based on LLVM's lit.
    
    	http://blog.llvm.org/2009/12/lit-it.html

---
+++ tests/int/malloc_array.c
@@ -0,0 +1,16 @@
+//RUN: %kcc -m32 -o - %s | %intsat --check-prefix=exp
+
+#include <stdlib.h>
+#include <stdint.h>
+
+void *malloc_array_nc(size_t n, size_t size)
+{
+	return malloc(n * size); // exp: {{umul}}
+}
+
+void *malloc_array(size_t n, size_t size)
+{
+	if (size && n > SIZE_MAX / size)
+		return NULL;
+	return malloc(n * size);
+}
