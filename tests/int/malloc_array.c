41519694e8cd8a01ba3ce27ed17300c8f2c2cd27
Xi Wang <xi@mit.edu>
2012-05-18 16:21:46 -0400

    pass malloc_array_2: x != (x * y) / y
    Add -sink after -overflow.

--- tests/int/malloc_array.c
+++ tests/int/malloc_array.c
@@ -11,6 +11,7 @@ void *malloc_array_nc(size_t n, size_t size)
 
 void *malloc_array_0(size_t n, size_t size)
 {
+	// need -overflow to recognize the idiom.
 	if (size && n > SIZE_MAX / size)
 		return NULL;
 	return malloc(n * size);
@@ -23,4 +24,14 @@ void *malloc_array_1(size_t n, size_t size)
 	if (n && size > SIZE_MAX / n)
 		return NULL;
 	return malloc(n * size);
+}
+
+void *malloc_array_2(size_t n, size_t size)
+{
+	// need -overflow to recognize the idiom.
+	// need to sink the multiplication after the check.
+	size_t bytes = n * size;
+	if (size && n != bytes / size)
+		return NULL;
+	return malloc(bytes);
 }
