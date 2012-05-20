c21c4851110227a2878f00b124f0c5bea18c2441
Xi Wang <xi@mit.edu>
2012-05-19 23:38:25 -0400

    add nouveau_dp to cmp tests

---
+++ tests/cmp/nouveau_dp.c
@@ -0,0 +1,20 @@
+// RUN: %kcc -o - %s | %cmpsat --check-prefix=exp
+// http://git.kernel.org/linus/44ab8cc56c45ca781371a4a77f35da19cf5db028
+
+#define DP_TRAIN_PRE_EMPHASIS_9_5	(3 << 3)
+#define DP_TRAIN_VOLTAGE_SWING_1200	(3 << 0)
+
+typedef unsigned char u8;
+
+int foo(u8 lane)
+{
+	u8 lpre = (lane & 0x0c) >> 2;
+	u8 lvsw = (lane & 0x03) >> 0;
+	if (lpre == DP_TRAIN_PRE_EMPHASIS_9_5) // exp: {{comparison always false}}
+		return -1;
+	if ((lpre << 3) == DP_TRAIN_PRE_EMPHASIS_9_5)
+		return -1;
+	if (lvsw == DP_TRAIN_VOLTAGE_SWING_1200)
+		return -2;
+	return 0;
+}
