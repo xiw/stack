27666737cbf8746e239d81662c59483c7326b70f
Xi Wang <xi@mit.edu>
2012-08-17 14:28:30 -0400

    add divisor zero check before -overflow
    Since -overflow rewrites expressions like x > N / y to multiplication,
    to avoid missing bugs we add a new pass -int-div to insert checks
    like y == 0 before that.

---
+++ src/IntDiv.cc
@@ -0,0 +1,50 @@
+#define DEBUG_TYPE "int-div"
+#include <llvm/IRBuilder.h>
+#include <llvm/Module.h>
+#include <llvm/Pass.h>
+#include <llvm/ADT/SmallVector.h>
+#include <llvm/Support/InstIterator.h>
+
+using namespace llvm;
+
+namespace {
+
+struct IntDiv : FunctionPass {
+	static char ID;
+	IntDiv() : FunctionPass(ID) { }
+
+	virtual bool runOnFunction(Function &);
+
+private:
+	Value *insertDivCheck(Value *);
+};
+
+} // anonymous namespace
+
+void insertIntTrap(Value *V, Instruction *IP, Pass *P);
+
+bool IntDiv::runOnFunction(Function &F) {
+	SmallVector<std::pair<Value *, Instruction *>, 4> Checks;
+	for (inst_iterator i = inst_begin(F), e = inst_end(F); i != e; ++i) {
+		Instruction *I = &*i;
+		switch (I->getOpcode()) {
+		default: continue;
+		case Instruction::SDiv:
+		case Instruction::UDiv:
+			break;
+		}
+		IRBuilder<> Builder(I);
+		Value *V = Builder.CreateIsNull(I->getOperand(1));
+		Checks.push_back(std::make_pair(V, I));
+	}
+	// Since inserting trap will change the control flow, it's better
+	// to do it after looping over all instructions.
+	for (size_t i = 0, n = Checks.size(); i != n; ++i)
+		insertIntTrap(Checks[i].first, Checks[i].second, this);
+	return !Checks.empty();
+}
+
+char IntDiv::ID;
+
+static RegisterPass<IntDiv>
+X("int-div", "Insert divisor zero checks", false, false);
