66ebc9c04055ff31bfee6527894ac69ef693b160
Xi Wang <xi.wang@gmail.com>
2012-09-03 01:08:14 -0400

    add CVE-2011-1759
    This one is missing from previous CVE collections.

---
+++ tests/linux/oabi-2011-1759.c
@@ -0,0 +1,29 @@
+// RUN: %linuxcc -m32 %s | kint | kdiff %s --prefix=exp32
+// RUN: %linuxcc -m64 %s | kint | kdiff %s
+// RUN: %linuxcc -D__PATCH__ -m32 %s | kint | kdiff %s
+// RUN: %linuxcc -D__PATCH__ -m64 %s | kint | kdiff %s
+// http://git.kernel.org/linus/0f22072ab50cac7983f9660d33974b45184da4f9
+
+#include "linux.h"
+
+struct sembuf {
+	unsigned short	sem_num;
+	short		sem_op;
+	short		sem_flg;
+};
+
+long sys_oabi_semtimedop(unsigned nsops)
+{
+	struct sembuf *sops;
+
+#ifndef __PATCH__
+	if (nsops < 1)
+#else
+	if (nsops < 1 || nsops > SEMOPM)
+#endif
+		return -EINVAL;
+	sops = kmalloc(sizeof(*sops) * nsops, GFP_KERNEL); // exp32: {{umul}}
+	if (!sops)
+		return -ENOMEM;
+	return 0;
+}
