57fc88ccd12f5114ebda007268b8747eb20ac39b
Xi Wang <xi@mit.edu>
2012-12-20 23:29:14 -0500

    add bugon-undef
    Insert bugon calls for uninitialized values.

---
+++ src/BugOnUndef.cc
@@ -0,0 +1,59 @@
+// Insert bug assertions on undef (e.g., uninitialized) value.
+
+#define DEBUG_TYPE "bugon-undef"
+#include "BugOn.h"
+#include <llvm/Constants.h>
+#include <llvm/Function.h>
+#include <llvm/Support/CallSite.h>
+
+using namespace llvm;
+
+namespace {
+
+struct BugOnUndef : BugOnPass {
+	static char ID;
+	BugOnUndef() : BugOnPass(ID) {}
+
+	virtual bool visit(Instruction *);
+};
+
+} // anonymous namespace
+
+static bool isDeadArg(Instruction *I, unsigned Idx) {
+	CallSite CS(I);
+	if (!CS)
+		return false;
+	Function *F = CS.getCalledFunction();
+	if (!F || F->empty() || Idx >= F->arg_size())
+		return false;
+	Function::arg_iterator A = F->arg_begin();
+	for (unsigned i = 0; i < Idx; ++i)
+		++A;
+	return A->use_empty();
+}
+
+bool BugOnUndef::visit(Instruction *I) {
+	// It's okay to have undef in phi's operands.
+	// TODO: catch conditional undefs.
+	if (isa<PHINode>(I) || isa<SelectInst>(I))
+		return false;
+	if (isa<InsertValueInst>(I) || isa<InsertElementInst>(I))
+		return false;
+	// If any operand is undef, this instruction must not be reachable.
+	for (unsigned i = 0, n = I->getNumOperands(); i != n; ++i) {
+		Value *V = I->getOperand(i);
+		if (isa<UndefValue>(V)) {
+			// Allow undef arguments created by -deadargelim,
+			// which are basically unused in the function body.
+			if (isDeadArg(I, i))
+				continue;
+			return insert(Builder->getTrue(), "undef");
+		}
+	}
+	return false;
+}
+
+char BugOnUndef::ID;
+
+static RegisterPass<BugOnUndef>
+X("bugon-undef", "Insert bugon calls for undef values");
