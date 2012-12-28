f8417de26a281ddb866510f83ee4c08419270db1
Xi Wang <xi@mit.edu>
2012-12-28 14:16:08 -0500

    fix build on ben
    In 3.2 release InstVisitor.h is still in llvm/Support.

--- src/llvm/InstVisitor.h
+++ src/llvm/InstVisitor.h
@@ -2,7 +2,7 @@
 
 #include <llvm/Config/config.h>
 
-#if LLVM_VERSION_MAJOR == 3 && LLVM_VERSION_MINOR < 2
+#if LLVM_VERSION_MAJOR == 3 && LLVM_VERSION_MINOR < 3
 #include <llvm/Support/InstVisitor.h>
 #else
 #include_next <llvm/InstVisitor.h>
