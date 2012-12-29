e0aeec1b55812df62bcbcfe067e1a392a3cca086
Xi Wang <xi@mit.edu>
2012-12-29 17:54:13 -0500

    add signed shift test case
    This is the runner up from the UB contest.  It also happened to
    glibc's __ieee754_fmodf() function.  The corresponding rule is that
    signed left shift x << y must have the same sign as that of x.
    
    Note that currently clang doesn't emit the nsw flag for shl, and
    thus we cannot really distinguish between signed and unsigned shifts.
    This test uses sed to add the flag as a workaround.

---
+++ test/antiopt/shlnsw.c
@@ -0,0 +1,14 @@
+// RUN: %cc %s | sed 's/shl i32/shl nsw i32/' | antiopt -S -loop-prepare -loop-rotate -bugon-int -bugon-loop -anti-dce -simplifycfg | FileCheck %s
+//
+// http://blog.regehr.org/archives/767
+// http://gcc.gnu.org/bugzilla/show_bug.cgi?id=54027
+
+// CHECK:      define i32 @foo()
+// CHECK-NEXT: entry:
+// CHECK-NEXT:   unreachable
+int foo()
+{
+  int x = 1;
+  while (x) x <<= 1;
+  return x;
+}
