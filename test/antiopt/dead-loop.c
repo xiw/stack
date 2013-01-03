ad0db88e40de65294e8ec73f1bc3982c820e8bb5
Xi Wang <xi@mit.edu>
2013-01-02 20:43:56 -0500

    generate better path condition with loop-rotate
    See dead-loop.c for an example.

---
+++ test/antiopt/dead-loop.c
@@ -0,0 +1,18 @@
+// RUN: %cc %s | optck | diagdiff --prefix=exp %s
+//
+// From krb5, krb5_ccache_copy() in clients/ksu/ccache.c.
+
+void bar(void);
+
+void foo()
+{
+	int *p = 0;
+	int i = 0;
+	if (p) {
+		while (p[i]) { // exp: {{bugon-null}}{{bugon-bounds}}
+			bar();
+			++i;
+		}
+	}
+}
+
