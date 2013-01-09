dd39fb83b93d5b7a06a3c43ff53c49442d5b1200
Nickolai Zeldovich <nickolai@csail.mit.edu>
2013-01-09 10:51:00 -0500

    fix -bugon-alias, handles malloc & realloc

--- src/BugOnAlias.cc
+++ src/BugOnAlias.cc
@@ -3,9 +3,12 @@
 #define DEBUG_TYPE "bugon-alias"
 #include "BugOn.h"
 #include <llvm/Analysis/Dominators.h>
+#include <llvm/Analysis/ValueTracking.h>
 #include <llvm/IR/Function.h>
 #include <llvm/IR/Instructions.h>
+#include <llvm/Support/InstIterator.h>
 #include <algorithm>
+#include <set>
 
 using namespace llvm;
 
@@ -21,6 +24,7 @@ struct BugOnAlias : BugOnPass {
 	virtual void getAnalysisUsage(AnalysisUsage &AU) const {
 		super::getAnalysisUsage(AU);
 		AU.addRequired<DominatorTree>();
+		AU.addRequired<DataLayout>();
 	}
 	virtual bool runOnFunction(Function &);
 
@@ -28,22 +32,38 @@ struct BugOnAlias : BugOnPass {
 
 private:
 	DominatorTree *DT;
+	DataLayout *DL;
+	std::set<Value*> Objects;
 
 	bool insertNoAlias(Value *, Value *);
-	bool visitICmpInst(ICmpInst *);
+	bool visitCallInst(CallInst *);
 };
 
 } // anonymous namespace
 
 bool BugOnAlias::runOnFunction(Function &F) {
-        DT = &getAnalysis<DominatorTree>();
-        return super::runOnFunction(F);
+	DT = &getAnalysis<DominatorTree>();
+	DL = &getAnalysis<DataLayout>();
+
+	// Find all of the pointers first.
+	Objects.clear();
+	for (inst_iterator i = inst_begin(F), e = inst_end(F); i != e; ) {
+		Instruction *I = &*i++;
+		if (I->getDebugLoc().isUnknown())
+			continue;
+		if (!I->getType()->isPointerTy())
+			continue;
+		Value *O = GetUnderlyingObject(I, DL, 0);
+		Objects.insert(O);
+	}
+
+	return super::runOnFunction(F);
 }
 
 bool BugOnAlias::runOnInstruction(Instruction *I) {
 	bool Changed = false;
-	if (ICmpInst *ICI = dyn_cast<ICmpInst>(I))
-		Changed |= visitICmpInst(ICI);
+	if (CallInst *CI = dyn_cast<CallInst>(I))
+		Changed |= visitCallInst(CI);
 	return Changed;
 }
 
@@ -54,46 +74,42 @@ bool BugOnAlias::runOnInstruction(Instruction *I) {
 // Given p = malloc(...) and any pointer q, a bug condition right
 // after the malloc call is:
 //   p != NULL && p == q.
-bool BugOnAlias::visitICmpInst(ICmpInst *I) {
-	if (!I->isEquality())
-		return false;
-	Value *L = I->getOperand(0)->stripPointerCasts();
-	Value *R = I->getOperand(1)->stripPointerCasts();
-	if (!L->getType()->isPointerTy())
-		return false;
-	bool Changed = false;
-	Changed |= insertNoAlias(L, R);
-	Changed |= insertNoAlias(R, L);
-	return Changed;
-
-}
-
-// Insert L != NULL && L == R right after L's def.
-bool BugOnAlias::insertNoAlias(Value *L, Value *R) {
-	// Ignore invoke for now.
-	CallInst *CI = dyn_cast<CallInst>(L);
-	if (!CI)
-		return false;
-	Function *F = CI->getCalledFunction();
-	if (!F)
+bool BugOnAlias::visitCallInst(CallInst *I) {
+	if (!I->getCalledFunction() || !I->getType()->isPointerTy())
 		return false;
 	// Has noalias on return.
-	if (!F->doesNotAlias(0))
+	if (!I->getCalledFunction()->doesNotAlias(0))
 		return false;
-	// To insert L == R, R's def has to domminate L.
-	if (Instruction *RI = dyn_cast<Instruction>(R)) {
-		if (!DT->dominates(RI, CI))
-			return false;
+
+	// Move insert point to after the noalias call.
+	Instruction *OldIP = setInsertPointAfter(I);
+
+	Value *NoaliasCond = createIsNotNull(I);
+	// XXX hack: handle realloc.  Would be nice to have this table:
+	// http://llvm.org/docs/doxygen/html/MemoryBuiltins_8cpp.html#af3fd8cba508798476dad01815e23711f
+	if (I->getCalledFunction()->getName() == "realloc") {
+		Value *Prev = I->getArgOperand(0);
+		NoaliasCond = createAnd(NoaliasCond, createIsNotNull(Prev));
+		NoaliasCond = createAnd(NoaliasCond,
+			Builder->CreateICmpNE(I, Prev));
 	}
-	// Move insert point to the noalias call L.
-	Instruction *IP = setInsertPointAfter(CI);
-	Value *V = createAnd(
-		createIsNotNull(L),
-		Builder->CreateICmpEQ(L, Builder->CreatePointerCast(R, L->getType()))
-	);
-	bool Changed = insert(V, "noalias");
+
+	bool Changed = false;
+	for (Value *V: Objects) {
+		Instruction *VI = dyn_cast<Instruction>(V);
+		if (VI != NULL && !DT->dominates(VI, I))
+			continue;
+		Value *E = createAnd(
+			NoaliasCond,
+			Builder->CreateICmpEQ(I,
+				Builder->CreatePointerCast(V, I->getType()))
+		);
+		Changed |= insert(E, "noalias");
+	}
+
 	// Restore the insert point.
-	setInsertPoint(IP);
+	setInsertPoint(OldIP);
+
 	return Changed;
 }
 
