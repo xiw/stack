ebb6b1901586f0cbc8abf66063ffc1535cc4279a
Nickolai Zeldovich <nickolai@csail.mit.edu>
2013-03-02 22:40:38 -0500

    add a global timeout (default to 1000 seconds in poptck)

---
+++ src/GlobalTimeout.cc
@@ -0,0 +1,63 @@
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <llvm/Pass.h>
+#include <llvm/Support/CommandLine.h>
+
+using namespace llvm;
+
+static cl::opt<unsigned>
+GlobalTimeoutOpt("global-timeout-sec",
+		 cl::desc("Specify a global timeout for entire analysis"),
+		 cl::value_desc("seconds"));
+
+namespace {
+
+static void global_check(union sigval) {
+	struct rusage ru_self, ru_child;
+	getrusage(RUSAGE_SELF, &ru_self);
+	getrusage(RUSAGE_CHILDREN, &ru_child);
+	if (ru_self.ru_utime.tv_sec + ru_child.ru_utime.tv_sec > GlobalTimeoutOpt) {
+		printf("Global timeout: self %ld.%06ld, child %ld.%06ld\n",
+			ru_self.ru_utime.tv_sec, ru_self.ru_utime.tv_usec,
+			ru_child.ru_utime.tv_sec, ru_child.ru_utime.tv_usec);
+		exit(-1);
+	}
+}
+
+struct GlobalTimeout : ImmutablePass {
+	static char ID;
+	GlobalTimeout() : ImmutablePass(ID) {
+		if (!GlobalTimeoutOpt)
+			return;
+
+		timer_t tid;
+		struct sigevent sigev;
+		sigev.sigev_notify = SIGEV_THREAD;
+		sigev.sigev_notify_function = &global_check;
+		sigev.sigev_notify_attributes = nullptr;
+		int r = timer_create(CLOCK_MONOTONIC, &sigev, &tid);
+		if (r < 0) {
+			perror("timer_create");
+			return;
+		}
+
+		struct itimerspec its;
+		its.it_value.tv_sec = 60;
+		its.it_value.tv_nsec = 0;
+		its.it_interval = its.it_value;
+		r = timer_settime(tid, 0, &its, nullptr);
+		if (r < 0) {
+			perror("timer_settime");
+			return;
+		}
+	}
+};
+
+} // anonymous namespace
+
+char GlobalTimeout::ID;
+
+static RegisterPass<GlobalTimeout>
+X("enable-global-timeout", "Terminate process after a fixed timeout (including child time)");
